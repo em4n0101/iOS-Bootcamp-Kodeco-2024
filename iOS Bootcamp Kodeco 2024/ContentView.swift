e7cbc972701002dd52787c6eb925b05a2a7520de
Alejandro Barranco <alexbar10@live.com.mx>
2024-01-29 02:25:46 -0600

    Initial Commit

---
+++ iOS Bootcamp Kodeco 2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Bootcamp Kodeco 2024
+//
+//  Created by Alejandro Barranco on 29/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Bootcamp Kodeco 2024/iOS_Bootcamp_Kodeco_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Bootcamp_Kodeco_2024App.swift
+//  iOS Bootcamp Kodeco 2024
+//
+//  Created by Alejandro Barranco on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Bootcamp_Kodeco_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
