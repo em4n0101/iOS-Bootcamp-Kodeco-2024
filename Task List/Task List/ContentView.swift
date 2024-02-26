571b2008f7d79c2040e832c5595b88711e5c8732
Alejandro Barranco <alexbar10@live.com.mx>
2024-02-26 12:22:45 -0600

    Week 5 Assignment

---
+++ Task List/Task List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task List
+//
+//  Created by Alejandro Barranco on 26/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Task List/Task List/Task_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ListApp.swift
+//  Task List
+//
+//  Created by Alejandro Barranco on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Task_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
